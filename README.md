4e9b56a199ff080d1f855f48c63e55d78adc1dfc
lkb <tmwlak@gmail.com>
2022-02-13 23:07:30 +0900

    .

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-
 # Tailwind CSS & Elm boilerplate
 
-### Prerequisite
+### How to run
 
 install Elm 0.19.1 using [elm-version-manager](https://github.com/mattludwigs/elm-version-manager)
 
 install create-elm-app via `yarn add create-elm-app`
 
-install via `yarn`
+install dependencies via `yarn`
 
-### Script
-Run `npm start` will start the project
-Run `npm run refresh` will generate new css based on your input.css and tailwind config.
+`yarn start` will start dev server
--- src/Main.elm
+++ src/Main.elm
@@ -1,41 +1,93 @@
 module Main exposing (..)
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src, class)
+import Html exposing (..)
+import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
+import Http
+import Json.Encode as Encode
 
 ---- MODEL ----
-
-type alias Model =
-    {}
-
+type alias Model = List String
 init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
+init = ( ["엉덩국", "김삿갓"], Cmd.none )
 
 ---- UPDATE ----
+type Msg 
+    = SendHttpRequest
+    | DataReceived (Result Http.Error String)
 
-type Msg
-    = NoOp
+url : String
+url =
+    "https://enabled-haddock-43.hasura.app/v1/graphql"
+
+getNicknames : Cmd Msg
+getNicknames =
+    let
+        headers = 
+            [Http.header "content-type" "application/json" 
+            , Http.header "x-hasura-admin-secret" "Yu5oC3d8brqEBcWVs641WMrU26PaYSoWBTr5rriloATN089IbI4RhAgv4Bvsf9TZ"
+            ]
+                    
+        body = Http.jsonBody (Encode.object
+                [ ( "query", Encode.string """query {
+                            LIVE_CODING_MOUM {
+                                username
+                                date
+                                duration
+                                stack
+                                thumbnail
+                                url
+                                title
+                            }
+                        }                   
+                """ )
+                ])
+      
+    in
+    Http.request
+        { method = "POST"
+        , url = url
+        , headers = headers
+        , expect = Http.expectString DataReceived
+        , body = body
+        , timeout = Nothing
+        , tracker = Nothing
+        }
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        SendHttpRequest ->
+            ( model, getNicknames )
+        DataReceived (Ok responseStr) ->
+            let
+                nicknames =
+                    String.split "," responseStr
+            in
+            ( nicknames, Cmd.none )
+
+        DataReceived (Err _) ->
+            ( model, Cmd.none )
 
 ---- VIEW ----
-
 view : Model -> Html Msg
 view model =
-    div [class "flex items-center justify-center w-full h-full"]
-        [ h1 [class "text-4xl p-5 font-bold bg-gray-200"] [ text "Elm에 오신것을 환영합니다" ]
+    div [class "flex flex-col justify-center items-center h-full"]
+        [ button [ onClick SendHttpRequest ]
+            [ text "Get data from server" ]
+        , h2 [class "text-5xl font-extrabold"] [ text "Old School Main Characters" ]
+        , ul [class "text-3xl"] (List.map viewNickname model)
         ]
 
----- PROGRAM ----
+viewNickname : String -> Html Msg
+viewNickname nickname = li [] [ text nickname ]
 
+---- PROGRAM ----
 main : Program () Model Msg
 main =
     Browser.element
         { view = view
-        , init = \_ -> init
         , update = update
+        , init = \_ -> init
         , subscriptions = always Sub.none
         }
